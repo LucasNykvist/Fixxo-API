680f331a04ad615663ac8b054159c7df8862704d
Lucas Nykvist <lucasnykvist01@gmail.com>
2022-11-27 02:03:18 +0100

    added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,11 +3,6 @@ const controller = express.Router()
 const { uuid } = require('uuidv4');
 let users = require("../data/simulated_database");
 
-controller.param("id", (req, res, next, id) => {
-    req.user = users.find((user) => user.id === id)
-    next()
-})
-
 controller.post("/", (req, res) => {
     let user = {
         firstName: req.body.firstName,
@@ -33,27 +28,30 @@ controller.get("/:id", (req, res) => {
 })
 
 controller.put("/:id", (req, res) => {
-    if (req != undefined) {
-        users.forEach(user => {
-            if (user.id === req.user.id) {
-                user.firstName = req.body.firstName ? req.body.firstName : user.firstName
-                user.lastName = req.body.lastName ? req.body.lastName : user.lastName
-                user.email = req.body.email ? req.body.email : user.email
-            }
-        })
-        res.status(200).json(res.user)
-    } else {
-        res.status(404).json()
+    const { id } = req.params
+
+    const { firstName, lastName, email } = req.body
+
+    const foundUser = users.find((user) => user.id === id)
+
+    if (firstName) {
+        foundUser.firstName = firstName
     }
+    if (lastName) {
+        foundUser.lastName = lastName
+    }
+    if (email) {
+        foundUser.email = email
+    }
+
+    res.status(200)
+
 })
 
 controller.delete("/:id", (req, res) => {
-    if (req != undefined) {
-        users = users.filter(user => user.id !== req.user.id)
-        res.status(204).json()
-    } else {
-        res.status(404).json()
-    }
+    const { id } = req.params
+    users = users.filter((user) => user.id !== id)
+    res.status(202).send(`User with id ${id} was deleted`)
 })
 
 module.exports = controller
