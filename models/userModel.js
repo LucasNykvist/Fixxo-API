987e0382136cc5629e32e1f87fead3edff7e85f6
Lucas Nykvist <lucasnykvist01@gmail.com>
2022-12-18 16:10:31 +0100

    made usermodel + schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,70 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
 
+// Schema för users
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    age: Number,
-    email: String,
-    password: String
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 })
 
-module.exports = mongoose.model("user", userSchema)
+// Static signup method
+userSchema.statics.signup = async function (email, password) {
+
+    // Validation
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+    if (!validator.isEmail(email)) {
+        throw Error("Email not vaild")
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password Not Strong Enough")
+    }
+
+    // Kolla om email är unik
+    const exists = await this.findOne({ email })
+    if (exists) {
+        throw new Error("Email already in use")
+    }
+
+    // Generera hashed password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    // Skapa user med krypterat password
+    const user = await this.create({ email, password: hash })
+    return user
+}
+
+// Static login method
+userSchema.statics.login = async function (email, password) {
+    // Validation
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error("Incorrect Email")
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error("Incorrect Password")
+    }
+
+    return user
+}
+
+
+
+module.exports = new mongoose.model("user", userSchema)
